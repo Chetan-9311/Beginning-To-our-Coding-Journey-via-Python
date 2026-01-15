ce39d8d669ccdb86b79eb90c110fee4c395e03b1
Chetan Wagh <waghdeepali964@gmail.com>
2026-01-15 23:00:25 +0530

    Add files via upload
    Created on 15th of Jan 2026 on 11:00 PM.

---
+++ Type_function_&_typecasting.py
@@ -0,0 +1,20 @@
+a=("45")
+b=int(a)  #string to integer conversion
+c=type(b)
+print(c)
+
+a=(45)
+b=str(a)  #integer to string conversion
+c=type(b)
+print(c)
+
+a=(45)
+b=float(a)  #integer to float conversion
+c=type(b)
+print(c)
+
+a="45"
+b=float(a)  #string to float conversion
+c=type(b)
+print(c)
+
