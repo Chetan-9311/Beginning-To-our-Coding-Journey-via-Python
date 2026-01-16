1ac4bd701e917e0fe924e0aa4adf70d1ebc84504
Chetan Wagh <waghdeepali964@gmail.com>
2026-01-16 23:37:17 +0530

    Uploaded on 16 Jan 2025 11:36 PM.

---
+++ input_function (1).py
@@ -0,0 +1,13 @@
+a=input("Enter No 1 or anything that you want:")
+b=input("Enter No 2 or anything that you want:")
+
+print(a+b) #this we use when we use to add two things
+#here the output will be added , not sum
+
+
+#but if we need to add or anything by two numbers and if we want sum then we will use:
+
+
+a=int(input("Enter No 1 or anything that you want:"))
+b=int(input("Enter No 2 or anything that you want:"))
+print("The sum of these two numbers is:" , a+b)
